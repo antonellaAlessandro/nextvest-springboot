3e66b39feebef181b36e48b8060292ea375ebc5e
antoalessandro <antonella.alessandro@davinci.edu.ar>
2026-09-15 09:34:11 -0300

    chore: configurar application.properties con conexión a PostgreSQL

---
+++ src/main/java/nextvest/NextvestApplication.java
@@ -0,0 +1,13 @@
+package nextvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nextvest/NextvestApplicationTests.java
@@ -0,0 +1,13 @@
+package nextvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
