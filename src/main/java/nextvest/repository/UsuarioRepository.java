f1b372970aa66f2e108240e1760a2bb133a48598
antoalessandro <antonella.alessandro@davinci.edu.ar>
2026-09-15 10:25:44 -0300

    Create UsuarioRepository.java
    feat: agregar UsuarioRepository

---
+++ src/main/java/nextvest/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package nextvest.repository;
+
+import nextvest.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByDni(Integer dni);
+}
